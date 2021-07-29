94e5ae23c980653ab0add956c0a78f7164e43dfc
Jeremiah DeGreeff <degreeff@mit.edu>
2021-07-28 23:37:20 -0700

    make ASE dependency soft

--- src/io/ase_trajectory.jl
+++ src/io/ase_trajectory.jl
@@ -1,12 +1,17 @@
 function write_trajectory(result::MolecularDynamicsResult, element::DFTK.Element, lattice::AbstractArray{Quantity, 2}, filename::String)
-    if isfile(filename)
-        println("replacing $(filename)")
-        rm(filename)
+    ase = pyimport_e("ase")
+    if ispynull(ase)
+        println("ASE is not installed, skipping write_trajectory")
+    else
+        if isfile(filename)
+            println("replacing $(filename)")
+            rm(filename)
+        end
+        traj = ase.io.trajectory.Trajectory(filename, "a")
+        for t ∈ 1:length(get_time_range(result))
+            state = get_bodies(result, t)
+            traj.write(ASEAtoms(state, element, lattice).atoms)
+        end
+        traj.close()
     end
-    traj = pyimport("ase.io.trajectory").Trajectory(filename, "a")
-    for t ∈ 1:length(get_time_range(result))
-        state = get_bodies(result, t)
-        traj.write(ASEAtoms(state, element, lattice).atoms)
-    end
-    traj.close()
 end
