7139226701663b77e5f3d6f099076cd32fc7d7b8
Jeremiah DeGreeff <degreeff@mit.edu>
2021-09-09 00:17:06 -0400

    rename subpackages

--- src/Atomistic.jl
+++ src/Atomistic.jl
@@ -16,14 +16,14 @@ export MassBodies, DFTKAtoms, ASEAtoms
 include("bodies.jl")
 
 export NuclearPotentialParameters, forces, potential_energy, LJParameters
-include("nuclear_potentials/abstractions.jl")
+include("interatomic_potentials/abstractions.jl")
 export DFTKParameters, dftk_atoms, analyze_convergence
-include("nuclear_potentials/dftk_integration.jl")
+include("interatomic_potentials/dftk_integration.jl")
 
 export MolecularDynamicsParameters, MolecularDynamicsResult, simulate, get_bodies, get_time_range, plot_temperature, plot_temperature!, plot_energy, plot_energy!, plot_rdf, calculate_rdf
-include("molecular_dynamics/abstractions.jl")
+include("simulators/abstractions.jl")
 export NBSParameters, NBSResult
-include("molecular_dynamics/nbs_integration.jl")
+include("simulators/nbs_integration.jl")
 
 export write_nbs_animation, write_ase_trajectory
 include("io/visualization.jl")
