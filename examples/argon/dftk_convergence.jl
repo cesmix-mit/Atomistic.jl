a624fd520290bccabf028767bbaa9fb89c20cb6e
Jeremiah DeGreeff <degreeff@mit.edu>
2022-04-06 21:33:04 -0400

    stop gap DFTK integration update

--- examples/argon/dftk_convergence.jl
+++ examples/argon/dftk_convergence.jl
@@ -6,7 +6,7 @@ using InteratomicPotentials
 using NBodySimulator
 using Plots
 
-setup_threading(n_blas = 4)
+setup_threading(n_blas=4)
 
 N = 8
 element = :Ar
@@ -16,35 +16,28 @@ thermostat_prob = 0.1 # this number was chosen arbitrarily
 Δt = 1e-2u"ps"
 
 initial_system = generate_atoms_in_cubic_cell(N, element, box_size, reference_temp)
-pspkey = list_psp(:Ar, functional = "lda")[1].identifier
+pspkey = list_psp(:Ar, functional="lda")[1].identifier
 for atom ∈ initial_system
     atom.data[:pseudopotential] = pspkey
 end
 
 eq_steps = 20000
 eq_thermostat = NBodySimulator.AndersenThermostat(austrip(reference_temp), thermostat_prob / austrip(Δt))
-eq_simulator = NBSimulator(Δt, eq_steps, thermostat = eq_thermostat)
+eq_simulator = NBSimulator(Δt, eq_steps, thermostat=eq_thermostat)
 potential = LennardJonesParameters(1.657e-21u"J", 0.34u"nm", 0.765u"nm")
 
 eq_result = @time simulate(initial_system, eq_simulator, potential)
 
-display(@time plot_rdf(eq_result, potential.σ))
+display(plot_rdf(eq_result, potential.σ))
 
 system = get_system(eq_result)
 cutoffs = (5:2.5:25)u"hartree"
 energies = @time map(cutoffs) do cutoff
     @info "Ecut: $(cutoff)"
-    dftk_potential = DFTKPotential(cutoff, [1, 1, 1]; damping = 0.7)
+    dftk_potential = DFTKPotential(cutoff, [1, 1, 1]; functionals=[:lda_x, :lda_c_pw], scf_kwargs=Dict(:damping => 0.7))
     InteratomicPotentials.potential_energy(system, dftk_potential)
 end
 
-display(plot(
-    title = "DFTK Convergence Analysis",
-    xlab = "Ecut",
-    ylab = "Total Energy",
-    legend = false,
-    cutoffs,
-    energies * u"hartree"
-))
+display(plot(title="DFTK Convergence Analysis", xlab="Ecut", ylab="Total Energy", legend=false, cutoffs, energies * u"hartree"))
 
 ;
--- examples/argon/molly_dftk_ab_initio_simulation.jl
+++ examples/argon/molly_dftk_ab_initio_simulation.jl
@@ -8,7 +8,7 @@ using GLMakie
 using InteratomicPotentials
 using Molly
 
-setup_threading(n_blas = 4)
+setup_threading(n_blas=4)
 
 N = 8
 element = :Ar
@@ -18,32 +18,33 @@ coupling_factor = 10 # this number was chosen arbitrarily
 Δt = 1e-2u"ps"
 
 initial_system = generate_atoms_in_cubic_cell(N, element, box_size, reference_temp)
-pspkey = list_psp(:Ar, functional = "lda")[1].identifier
+pspkey = list_psp(:Ar, functional="lda")[1].identifier
 for atom ∈ initial_system
     atom.data[:pseudopotential] = pspkey
 end
 
 eq_steps = 20000
 eq_thermostat = Molly.AndersenThermostat(reference_temp, Δt * coupling_factor)
-eq_simulator = MollySimulator(Δt, eq_steps, coupling = eq_thermostat)
+eq_simulator = MollySimulator(Δt, eq_steps, coupling=eq_thermostat)
 potential = InteratomicPotentials.LennardJones(austrip(1.657e-21u"J"), austrip(0.34u"nm"), austrip(0.765u"nm"), [:Ar])
 
 eq_result = @time simulate(initial_system, eq_simulator, potential)
 
-display(@time plot_temperature(eq_result, eq_simulator.steps ÷ 200))
-display(@time plot_energy(eq_result, eq_simulator.steps ÷ 200))
-display(@time plot_rdf(eq_result, potential.σ))
+display(plot_temperature(eq_result, eq_simulator.steps ÷ 200))
+display(plot_energy(eq_result, eq_simulator.steps ÷ 200))
+display(plot_rdf(eq_result, potential.σ))
 
 ab_initio_steps = 200
-ab_initio_simulator = MollySimulator(Δt, ab_initio_steps, t₀ = get_time(eq_result))
-dftk_potential = DFTKPotential(5u"hartree", [1, 1, 1]; damping = 0.7) # very non-physical but fast for demonstration purposes
+ab_initio_simulator = MollySimulator(Δt, ab_initio_steps, t₀=get_time(eq_result))
+# very non-physical but fast for demonstration purposes
+dftk_potential = DFTKPotential(5u"hartree", [1, 1, 1]; functionals=[:lda_x, :lda_c_pw], scf_kwargs=Dict(:damping => 0.7))
 
 ab_initio_result = @time simulate(get_system(eq_result), ab_initio_simulator, dftk_potential)
 
 # Plotting on separate plots because the timespan is so much smaller than in the first phase
-display(@time plot_temperature(ab_initio_result, 1))
-display(@time plot_energy(ab_initio_result, 1))
-display(@time plot_rdf(ab_initio_result, potential.σ))
+display(plot_temperature(ab_initio_result, 1))
+display(plot_energy(ab_initio_result, 1))
+display(plot_rdf(ab_initio_result, potential.σ))
 
 visualize(ab_initio_result, "artifacts/argon_ab_initio_molly.mp4")
 
