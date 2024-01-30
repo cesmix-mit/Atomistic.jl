13758e458a106d80b889277b0c2f4a4054bc48ea
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2024-01-30 17:54:00 -0500

    significantly downsizing package w/ comments for now

--- src/Atomistic.jl
+++ src/Atomistic.jl
@@ -1,50 +1,52 @@
 module Atomistic
 
-using Reexport
+#using Reexport
 
 using AtomsBase
+using Unitful
+using ExtXYZ
 using InteratomicPotentials
 
-using Distances
-using LinearAlgebra
+#using Distances
+#using LinearAlgebra
 using StaticArrays
-using Base.Threads
-@reexport using Unitful, UnitfulAtomic
+#using Base.Threads
+#@reexport using Unitful, UnitfulAtomic
 
 using Molly
-using NBodySimulator
+#using NBodySimulator
 
-using PeriodicTable
-using Plots, UnitfulRecipes
+#using PeriodicTable
+#using Plots, UnitfulRecipes
 
-import NBodySimulator: Body, BoundaryConditions, NullThermostat, SimulationResult, Thermostat
-import Plots: Plot
+#import NBodySimulator: Body, BoundaryConditions, NullThermostat, SimulationResult, Thermostat
+#import Plots: Plot
 
-include("unit_convention.jl")
+#include("unit_convention.jl")
 
 # Initialization Convenience functions
-export generate_atoms_in_cubic_cell
-include("initialization.jl")
+#export generate_atoms_in_cubic_cell
+#include("initialization.jl")
 
 # API
-include("api/exceptions.jl")
-export MolecularDynamicsResult
-export get_time_range, get_num_bodies, get_bounding_box, get_boundary_conditions, reference_temperature
-export get_time, get_positions, get_velocities, get_particles, get_system
-export temperature, kinetic_energy, potential_energy, total_energy, rdf
-include("api/md_result.jl")
-export MolecularDynamicsSimulator
-export simulate
-include("api/md_simulator.jl")
+#include("api/exceptions.jl")
+#export MolecularDynamicsResult
+#export get_time_range, get_num_bodies, get_bounding_box, get_boundary_conditions, reference_temperature
+#export get_time, get_positions, get_velocities, get_particles, get_system
+#export temperature, kinetic_energy, potential_energy, total_energy, rdf
+#include("api/md_result.jl")
+#export MolecularDynamicsSimulator
+#export simulate
+#include("api/md_simulator.jl")
 
 # Implementations
-export NBSimulator, NBSResult
-include("implementations/nbodysimulator.jl")
-export MollySimulator, MollyResult
+#export NBSimulator, NBSResult
+#include("implementations/nbodysimulator.jl")
+#export MollySimulator, MollyResult
 include("implementations/molly.jl")
 
 # Analysis
-export plot_temperature, plot_temperature!, plot_energy, plot_energy!, plot_rdf
-include("analysis/plotting.jl")
+#export plot_temperature, plot_temperature!, plot_energy, plot_energy!, plot_rdf
+#include("analysis/plotting.jl")
 
 end
--- src/implementations/molly.jl
+++ src/implementations/molly.jl
@@ -1,5 +1,17 @@
 # Implementation of Atomistic API with Molly.jl
 
-include("molly/molly_base.jl")
-include("molly/molly_simulator.jl")
-include("molly/molly_result.jl")
+#=
+The contents of molly_base.jl have largely been superceded by the native
+Molly.System(::AbstractSystem). However the new molly_temporary_utils.jl file is needed to
+ensure that the ExtXYZ.Atoms objects (returned by AtomsIO) has static arrays for pos and vels
+
+So one of two longer-term solutions are needed
+1. Modifying the validate_coords function in Molly to be able to handle Vector{Vector}
+   instead of just Vector{SArray}
+2. ExtXYZ.Atoms should use StaticArrays for positions and velocities
+=#
+#include("molly/molly_base.jl")
+include("molly/molly_temporary_utils.jl")
+#include("molly/molly_simulator.jl")
+#include("molly/molly_result.jl")
+include("molly/interpot_wrapper.jl")
